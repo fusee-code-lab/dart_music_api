c5b784f9b3ee49d4bc1ea47c41ff75bb4a769229
jctaoo <jctaoo@outlook.com>
2021-01-15 13:57:02 +0800

    first commit

---
+++ example/dart_music_api_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_music_api/dart_music_api.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/dart_music_api_test.dart
@@ -0,0 +1,16 @@
+import 'package:dart_music_api/dart_music_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
